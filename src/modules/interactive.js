ceec6b99e902a174bfa9791ace83fc6a67ff6ed2
Mike111222 <Tipu870@gmail.com>
2023-06-07 13:37:40 +0500

    Update interactive.js

--- src/modules/interactive.js
+++ src/modules/interactive.js
@@ -1,19 +1,13 @@
-// Importing the addTask function from './addTask.js'
+
 import addTask from './addTask.js';
 
-// Importing the removeTask function from './removeTask.js'
 import removeTask from './removeTask.js';
 
-// Importing the clearCompletedTask, toggleTaskStatus,
-// and editTask functions from './clearToggleEditTask.js'
 import { clearCompletedTask, toggleTaskStatus, editTask } from './clearToggleEditTask.js';
 
-// TaskList class
 // This class represents a task list and manages the functionality related to tasks.
 class TaskList {
   constructor() {
-    // Initializing the tasks array by retrieving it from localStorage,
-    // or creating an empty array if it doesn't exist.
     this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
     // Getting references to various elements in the DOM.
@@ -24,8 +18,7 @@ class TaskList {
     this.resetBtn = document.getElementById('reset');
 
     // Adding an event listener to the task form's submit event.
-    // When the form is submitted, a new task is added to the tasks array,
-    // and the tasks are saved and displayed.
+    
     this.taskForm.addEventListener('submit', (e) => {
       e.preventDefault();
       this.tasks = addTask(this.taskInput, this.tasks);
@@ -35,8 +28,7 @@ class TaskList {
     });
 
     // Adding an event listener to the clear completed button's click event.
-    // When the button is clicked, completed tasks are cleared from the tasks array,
-    // and the tasks are saved and displayed.
+    
     this.clearCompletedBtn.addEventListener('click', () => {
       this.tasks = clearCompletedTask(this.tasks);
       this.saveTasks();
@@ -44,8 +36,7 @@ class TaskList {
     });
 
     // Adding an event listener to the reset button's click event.
-    // When the button is clicked, all tasks are removed from the tasks array,
-    // and the tasks are saved and displayed.
+
     this.resetBtn.addEventListener('click', () => {
       this.tasks = [];
       this.saveTasks();
@@ -56,27 +47,22 @@ class TaskList {
     this.displayTasks();
   }
 
-  // editTask method
   // This method allows editing a task by calling the editTask function and
   // passing the necessary arguments.
-  // The tasks array is updated, and then the tasks are saved and displayed.
+  
   editTask(id, newName) {
     editTask(id, newName, this.tasks, this.saveTasks.bind(this), this.displayTasks.bind(this));
   }
 
-  // removeTask method
+ 
   // This method removes a task by calling the removeTask function
   // and passing the necessary arguments.
-  // The tasks array is updated, and then the tasks are saved and displayed.
   removeTask(id) {
     this.tasks = removeTask(id, this.tasks);
     this.saveTasks();
     this.displayTasks();
   }
 
-  // toggleTaskStatus method
-  // This method toggles the status of a task by calling the toggleTaskStatus
-  // function and passing the necessary arguments.
   // The tasks array is updated, and then the tasks are saved and displayed.
   toggleTaskStatus(id) {
     this.tasks = toggleTaskStatus(id, this.tasks);
@@ -84,29 +70,23 @@ class TaskList {
     this.displayTasks();
   }
 
-  // resetTasks method
   // This method removes all tasks from the tasks array.
-  // The tasks are saved and displayed.
+ 
   resetTasks() {
     this.tasks = [];
     this.saveTasks();
     this.displayTasks();
   }
 
-  // saveTasks method
   // This method saves the tasks array to localStorage by
   // converting it to a JSON string.
   saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
-  // displayTasks method
+ 
   // This method updates the task list in the DOM based on the tasks array.
-  // It iterates over the tasks array and creates DOM elements for each task,
-  // including checkboxes, task names, and buttons.
-  // Event listeners are attached to the checkboxes, edit buttons,
-  // and delete buttons for task manipulation.
-  // The updated task list is appended to the task list container in the DOM.
+  
   displayTasks() {
     this.taskList.innerHTML = '';
     for (let i = 0; i < this.tasks.length; i += 1) {
@@ -140,5 +120,4 @@ class TaskList {
   }
 }
 
-// Exporting the TaskList class as the default export for use in other parts of the code.
 export default TaskList;
