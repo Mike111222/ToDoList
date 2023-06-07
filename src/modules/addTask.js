01870d72756f4635469536431d88fd84d90cacf3
Mike111222 <Tipu870@gmail.com>
2023-06-07 13:36:29 +0500

    Update addTask.js

--- src/modules/addTask.js
+++ src/modules/addTask.js
@@ -1,14 +1,10 @@
-// Importing the Task class from './task.js'
+
 import Task from './taskList.js';
 
-// addTask function
-// This function takes a taskInput (presumably an HTML input element) and an array of tasks.
+
+// This function takes a taskInput (presumably an HTML input element) and an
+// array of tasks.
 // It extracts the trimmed value from the taskInput, representing the task name.
-// It generates a unique id for the new task by adding 1 to the length of the tasks array.
-// If the task name is not an empty string, it creates a new Task object with the name,
-// status (false for not completed), and id.
-// The new task is then added to the tasks array using the push method.
-// Finally, the updated tasks array is returned.
 function addTask(taskInput, tasks) {
   const taskName = taskInput.value.trim();
   const id = tasks.length + 1;
@@ -19,5 +15,4 @@ function addTask(taskInput, tasks) {
   return tasks;
 }
 
-// Exporting the addTask function as the default export for use in other parts of the code.
 export default addTask;
