c5979957a105c5f27cf3c92757e8b834ee1e8830
Mike111222 <Tipu870@gmail.com>
2023-06-07 13:47:01 +0500

    Update taskList.js

--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -1,11 +1,9 @@
-
 // This class represents a task object with a name, status, and id.
 class Task {
   constructor(name, status, id) {
-    
-    this.name = name; 
+    this.name = name;
     this.status = status;
-    this.id = id; 
+    this.id = id;
   }
 }
 
