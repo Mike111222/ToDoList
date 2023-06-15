97a2dac0034dc86c5a869a8603ac0d571177b70b
Ayomide <ayomidesunday31@gmail.com>
2023-06-15 08:59:58 +0100

    Format test files

--- tests/addTask.test.js
+++ tests/addTask.test.js
@@ -1,18 +1,16 @@
-// Import the necessary modules for the tests
 import Task from '../src/modules/taskList';
 import addTask from '../src/modules/addTask';
 
 
-
+// I think we should have 1-2 tests group together so we can easily find errors
 // Test suite for the addTask function
 describe('addTask function', () => {
   // Test case 1: Adding a new task to an empty array
   it('should add a new task to an empty array', () => {
-    // Arrange: Create an empty array and a task input object
+    
     const tasks = [];
     const taskInput = { value: 'Buy groceries' };
-
-    // Act: Call the addTask function
+    
     const result = addTask(taskInput, tasks);
 
     // Assert: Check that the task was added to the array
@@ -21,11 +19,9 @@ describe('addTask function', () => {
 
   // Test case 2: Adding a new task to a non-empty array
   it('should add a new task to a non-empty array', () => {
-    // Arrange: Create an array with one task and a task input object
     const tasks = [new Task('Buy groceries', false, 1)];
     const taskInput = { value: 'Do laundry' };
-
-    // Act: Call the addTask function
+    
     const result = addTask(taskInput, tasks);
 
     // Assert: Check that the task was added to the array
@@ -37,11 +33,10 @@ describe('addTask function', () => {
 
   // Test case 3: Not adding a new task if the input is empty
   it('should not add a new task if the input is empty', () => {
-    // Arrange: Create an array and an empty task input object
+   
     const tasks = [];
     const taskInput = { value: '' };
-
-    // Act: Call the addTask function
+   
     const result = addTask(taskInput, tasks);
 
     // Assert: Check that the array is still empty
--- tests/removeTask.test.js
+++ tests/removeTask.test.js
@@ -6,11 +6,9 @@ import addTask from '../src/modules/addTask.js';
 describe('addTask function', () => {
   // Test case 1: Adding a new task to an empty array
   it('should add a new task to an empty array', () => {
-    // Arrange: Create an empty array and a task input object
     const tasks = [];
     const taskInput = { value: 'Buy groceries' };
-
-    // Act: Call the addTask function
+    
     const result = addTask(taskInput, tasks);
 
     // Assert: Check that the task was added to the array
@@ -19,11 +17,9 @@ describe('addTask function', () => {
 
   // Test case 2: Adding a new task to a non-empty array
   it('should add a new task to a non-empty array', () => {
-    // Arrange: Create an array with one task and a task input object
     const tasks = [new Task('Buy groceries', false, 1)];
     const taskInput = { value: 'Do laundry' };
 
-    // Act: Call the addTask function
     const result = addTask(taskInput, tasks);
 
     // Assert: Check that the task was added to the array
@@ -35,11 +31,9 @@ describe('addTask function', () => {
 
   // Test case 3: Not adding a new task if the input is empty
   it('should not add a new task if the input is empty', () => {
-    // Arrange: Create an array and an empty task input object
     const tasks = [];
     const taskInput = { value: '' };
 
-    // Act: Call the addTask function
     const result = addTask(taskInput, tasks);
 
     // Assert: Check that the array is still empty
